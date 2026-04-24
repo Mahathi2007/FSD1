d5e64d7f4429de1111cfa13c9bf879f10d5c6e30
Mahathi <mahathitarugu@gmail.com>
2026-04-24 19:56:50 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: "https://fsd-backend-ya9i.onrender.com/api",
   headers: {
     'Content-Type': 'application/json'
   }
